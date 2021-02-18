3a0f617cf3fc05bf63b9e81d1067795d8c40af2e
onlyloveyd <onlyloveyd@gmail.com>
2021-02-18 15:55:15 +0800

    1.委托模式

---
+++ app/src/main/java/tech/kicky/delegate/DelegateMain.kt
@@ -0,0 +1,29 @@
+package tech.kicky.delegate
+
+/**
+ * 委托 Main
+ *
+ * @author yidong
+ * @date 2/18/21
+ */
+interface Base {
+    val message: String
+    fun print()
+}
+
+class BaseImpl(val x: Int) : Base {
+    override val message = "BaseImpl: x = $x"
+    override fun print() { println(message) }
+}
+
+class Derived(b: Base) : Base by b {
+    // 在 b 的 `print` 实现中不会访问到这个属性
+    override val message = "Message of Derived"
+}
+
+fun main() {
+    val b = BaseImpl(10)
+    val derived = Derived(b)
+    derived.print()
+    println(derived.message)
+}
--- app/src/main/java/tech/kicky/inlinefunc/InlineMain.kt
+++ app/src/main/java/tech/kicky/inlinefunc/InlineMain.kt
@@ -5,28 +5,25 @@ package tech.kicky.inlinefunc
  * author: yidong
  * 2021/2/17
  */
-class InlineMain {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            greeting({
-                println("Before")
-            }, {
-                println("After")
-            })
-            println("Finish Greeting")
-        }
 
-        private inline fun greeting(before: () -> Unit, crossinline after: () -> Unit) {
-            before()
-            println("Hello")
-            wrapAfter { after() }
-        }
-
-        private fun wrapAfter(after: () -> Unit) {
-            println("Before After")
-            after()
-        }
-    }
+fun main() {
+    greeting({
+        println("Before")
+    }, {
+        println("After")
+    })
+    println("Finish Greeting")
 }
+
+private inline fun greeting(before: () -> Unit, crossinline after: () -> Unit) {
+    before()
+    println("Hello")
+    wrapAfter { after() }
+}
+
+private fun wrapAfter(after: () -> Unit) {
+    println("Before After")
+    after()
+}
+
 
